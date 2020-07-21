d923991d214fa750b5366873cae1729d6d08a2a0
Marvin Beym <Marvin.Beym@gmx.de>
2020-07-21 14:50:09 +0200

    first commit

---
+++ ESP32/src/Configuration.h
@@ -0,0 +1,9 @@
+const int fan1_max_rpm = 3000;
+const int fan2_max_rpm = 1800;
+const int led2_numberOfLEDs = 60;
+const int led2_currentLimit = 1500;
+const int dht_1_min_tempGraph = -20;
+const int dht_2_min_tempGraph = -20;
+const long dht_sense_interval = 2000;
+const long display_update_interval = 1500;
+#define EEPROM_SIZE 7
