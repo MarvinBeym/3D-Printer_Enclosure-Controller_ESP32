46e10ac2cb83888993f9a1a06ba078296845a4fa
Marvin Beym <Marvin.Beym@gmx.de>
2021-04-06 13:00:58 +0200

    Added map function to helper file.

--- ESP32/interface/src/project/helper.js
+++ ESP32/interface/src/project/helper.js
@@ -8,6 +8,10 @@ export const fetchBackend = (fetchEndpoint) => {
 	})
 }
 
+export const map = (value, inMin, inMax, outMin, outMax) => {
+	return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
 export const useInterval = (callback, delay) => {
 	const savedCallback = useRef(callback)
 
