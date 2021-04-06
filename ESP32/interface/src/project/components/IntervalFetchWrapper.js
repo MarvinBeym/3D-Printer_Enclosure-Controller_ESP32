b3339ae7d1061a78ff390aec72295366ef16b01e
Marvin Beym <Marvin.Beym@gmx.de>
2021-04-06 15:59:28 +0200

    Improved IntervalFetchWrapper with showing an error message after x retries. Started working on the main page.

--- ESP32/interface/src/project/components/IntervalFetchWrapper.js
+++ ESP32/interface/src/project/components/IntervalFetchWrapper.js
@@ -1,17 +1,83 @@
-import {useState} from "react";
+import React, {useState} from "react";
 import {fetchBackend, useInterval} from "../helper";
+import {LinearProgress, Typography} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
+import clsx from "clsx";
 
+const useStyles = makeStyles((theme) => ({
+	loading: {
+		color: theme.palette.primary.main,
+		display: "flex",
+		flexDirection: "column",
+		justifyContent: "center",
+		alignItems: "center",
+		width: "100%",
+		height: "100%",
+		padding: "0 4rem",
+	},
+	loadingProgress: {
+		width: "100%",
+		margin: theme.spacing(4),
+	},
+	loadingDetails: {
+		fontSize: "1.8rem",
+		fontWeight: "bold",
+		textAlign: "center",
+	},
+	loadingFailure: {
+		fontSize: "1.8rem",
+		fontWeight: "bold",
+	}
+}));
 
-const IntervalFetchWrapper = ({fetchEndpoint, render, fetchInterval = 2000}) => {
+const LoadingFailure = ({message = "Fetch error"}) => {
+	const styles = useStyles();
+	return (
+		<div className={clsx(styles.loading, styles.loadingFailure)}>
+			{message}
+		</div>
+	)
+}
+
+const Loading = () => {
+	const styles = useStyles();
+	return (
+		<div className={styles.loading}>
+			<LinearProgress className={styles.loadingProgress}/>
+			<Typography variant="h6" className={styles.loadingDetails}>
+				Loading&hellip;
+			</Typography>
+		</div>
+	)
+}
+
+const IntervalFetchWrapper = ({
+								  fetchEndpoint,
+								  render,
+								  renderWithoutData = false,
+								  fetchInterval = 2000,
+								  retries = 5
+							  }) => {
 	const [fetchData, setFetchData] = useState(null);
-
+	const [tries, setTries] = useState(0);
 	useInterval(() => {
-		fetchBackend(fetchEndpoint).then((data) => {
-			setFetchData(data);
-		})
+		if (fetchEndpoint && fetchEndpoint !== "" && tries !== retries) {
+			fetchBackend(fetchEndpoint).then((data) => {
+				setFetchData(data);
+			}).catch((error) => {
+				setTries(tries + 1);
+			})
+		}
 	}, fetchInterval);
 
-	return render(fetchData);
+	if (fetchData || renderWithoutData) {
+		return render(fetchData);
+	} else if (tries >= retries) {
+		return <LoadingFailure message={"Failed to fetch after " + tries + " retries"}/>
+	}
+
+	return <Loading/>
+
 }
 
 export default IntervalFetchWrapper;
--- ESP32/interface/src/project/tabs/Main.js
+++ ESP32/interface/src/project/tabs/Main.js
@@ -1,12 +1,40 @@
-import React, {Component} from 'react';
+import React from 'react';
+import {makeStyles} from "@material-ui/core/styles";
+import PaperSection from "../components/PaperSection";
+import IntervalFetchWrapper from "../components/IntervalFetchWrapper";
 
-class Main extends Component {
+const useStyles = makeStyles((theme) => ({
+	main: {
+		display: "grid",
+		gridTemplateColumns: "auto auto",
+		gridTemplateRows: "auto auto",
+		gridGap: "1rem",
+	},
+	infoSection: {
+		margin: "0",
+	}
+}));
 
-    render() {
-        return (
-            <div>T</div>
-        )
-    }
+const Main = () => {
+	const styles = useStyles();
+	return (
+		<IntervalFetchWrapper fetchInterval={2000} fetchEndpoint="main/data" render={(data) => (
+			<PaperSection className={styles.main}>
+				<PaperSection paperClassName={styles.infoSection} title="Fan 1">
+
+				</PaperSection>
+				<PaperSection paperClassName={styles.infoSection} title="Fan 2">
+
+				</PaperSection>
+				<PaperSection paperClassName={styles.infoSection} title="Sensor 1">
+
+				</PaperSection>
+				<PaperSection paperClassName={styles.infoSection} title="Sensor 2">
+
+				</PaperSection>
+			</PaperSection>
+		)}/>
+	)
 
 }
 
