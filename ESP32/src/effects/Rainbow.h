405b9581a2d2dc04f50bc49b9701a54fdb55f734
Marvin Beym <Marvin.Beym@gmx.de>
2021-04-19 14:16:32 +0200

    Added effect config to rainbow effect.

--- ESP32/src/effects/Rainbow.h
+++ ESP32/src/effects/Rainbow.h
@@ -5,11 +5,22 @@ class Rainbow : public Effect
 	public:
 		Rainbow(const char *_name, bool _effectGetsHandledOnce = false) : Effect{_name, _effectGetsHandledOnce} {};
 
+		bool hasEffectConfig()
+		{
+			return true;
+		}
+
 		void effectHandler(CRGB *leds, int numberOfLeds)
 		{
-			uint8_t fillNumber = beat8(100, 300);
+			uint8_t fillNumber = beat8(
+					getNumberFieldValue("beatsPerMinute"),
+					300);
 			fill_rainbow(leds, numberOfLeds, fillNumber, 255 / numberOfLeds);
-			fadeToBlackBy(leds, numberOfLeds, 80);
 			FastLED.show();
+		}
+
+		void defineEffectConfig()
+		{
+			addNumberField("beatsPerMinute", "Beats per minute", 100, 0, 250);
 		}
 };
