81ddf10dc1c0a91bd6b0108bb6fb747d886b45b1
Marvin Beym <Marvin.Beym@gmx.de>
2021-04-24 19:06:46 +0200

    Add default page redirect from webinterface config state.

--- ESP32/interface/src/project/EnclosureController.js
+++ ESP32/interface/src/project/EnclosureController.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Redirect, Switch, useHistory} from 'react-router-dom'
 
 import {Tab, Tabs} from '@material-ui/core';
@@ -21,42 +21,48 @@ import {loadWebinterfaceConfigFromLocalStorage} from "./redux/reducers/configura
 store.dispatch(connect(process.env.REACT_APP_WEB_SOCKET_ROOT + "/ws", "ENCLOSURE_CONTROLLER"));
 
 const EnclosureController = ({location}) => {
-    const history = useHistory();
-    const handleTabChange = (event, path) => {
-        history.push(path);
-    };
+	const history = useHistory();
+	const [defaultPage, setDefaultPage] = useState(store.getState().configuration.webinterface.defaultPage);
+	const handleTabChange = (event, path) => {
+		history.push(path);
+	};
 
-    //Load from localstorage
-    useEffect(() => {
-        store.dispatch(loadWebinterfaceConfigFromLocalStorage());
-    }, []);
 
-    return (
-        <Provider store={store}>
-            <MenuAppBar sectionTitle="Enclosure Controller">
-                <Tabs value={location.pathname} onChange={handleTabChange} variant="fullWidth">
-                    <Tab value={`/${PROJECT_PATH}/enclosure-controller/information`} label="Information"/>
-                    <Tab value={`/${PROJECT_PATH}/enclosure-controller/main`} label="Main"/>
-                    <Tab value={`/${PROJECT_PATH}/enclosure-controller/fans`} label="Fans"/>
-                    <Tab value={`/${PROJECT_PATH}/enclosure-controller/sensors`} label="Sensors"/>
-                    <Tab value={`/${PROJECT_PATH}/enclosure-controller/leds`} label="Leds"/>
-                    <Tab value={`/${PROJECT_PATH}/enclosure-controller/configuration`} label="Configuration"/>
-                </Tabs>
-                <Switch>
-                    <AuthenticatedRoute exact path={`/${PROJECT_PATH}/enclosure-controller/information`}
-                                        component={Information}/>
-                    <AuthenticatedRoute exact path={`/${PROJECT_PATH}/enclosure-controller/main`} component={Main}/>
-                    <AuthenticatedRoute exact path={`/${PROJECT_PATH}/enclosure-controller/fans`} component={Fans}/>
-                    <AuthenticatedRoute exact path={`/${PROJECT_PATH}/enclosure-controller/sensors`}
-                                        component={Sensors}/>
-                    <AuthenticatedRoute exact path={`/${PROJECT_PATH}/enclosure-controller/leds`} component={Leds}/>
-                    <AuthenticatedRoute exact path={`/${PROJECT_PATH}/enclosure-controller/configuration`}
-                                        component={Configuration}/>
-                    <Redirect to={`/${PROJECT_PATH}/enclosure-controller/information`}/>
-                </Switch>
-            </MenuAppBar>
-        </Provider>
-    )
+	//Load from localstorage
+	useEffect(() => {
+		store.dispatch(loadWebinterfaceConfigFromLocalStorage());
+	}, []);
+
+	useEffect(() => {
+		setDefaultPage(store.getState().configuration.webinterface.defaultPage);
+	}, []);
+
+	return (
+		<Provider store={store}>
+			<MenuAppBar sectionTitle="Enclosure Controller">
+				<Tabs value={location.pathname} onChange={handleTabChange} variant="fullWidth">
+					<Tab value={`/${PROJECT_PATH}/enclosure-controller/information`} label="Information"/>
+					<Tab value={`/${PROJECT_PATH}/enclosure-controller/main`} label="Main"/>
+					<Tab value={`/${PROJECT_PATH}/enclosure-controller/fans`} label="Fans"/>
+					<Tab value={`/${PROJECT_PATH}/enclosure-controller/sensors`} label="Sensors"/>
+					<Tab value={`/${PROJECT_PATH}/enclosure-controller/leds`} label="Leds"/>
+					<Tab value={`/${PROJECT_PATH}/enclosure-controller/configuration`} label="Configuration"/>
+				</Tabs>
+				<Switch>
+					<AuthenticatedRoute exact path={`/${PROJECT_PATH}/enclosure-controller/information`}
+										component={Information}/>
+					<AuthenticatedRoute exact path={`/${PROJECT_PATH}/enclosure-controller/main`} component={Main}/>
+					<AuthenticatedRoute exact path={`/${PROJECT_PATH}/enclosure-controller/fans`} component={Fans}/>
+					<AuthenticatedRoute exact path={`/${PROJECT_PATH}/enclosure-controller/sensors`}
+										component={Sensors}/>
+					<AuthenticatedRoute exact path={`/${PROJECT_PATH}/enclosure-controller/leds`} component={Leds}/>
+					<AuthenticatedRoute exact path={`/${PROJECT_PATH}/enclosure-controller/configuration`}
+										component={Configuration}/>
+					<Redirect to={`/${PROJECT_PATH}${defaultPage}`}/>
+				</Switch>
+			</MenuAppBar>
+		</Provider>
+	)
 }
 
 export default EnclosureController;
