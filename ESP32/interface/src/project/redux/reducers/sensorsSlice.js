eeec45830c76ebd40996dc72d9d8bf2c047e07c8
Marvin Beym <Marvin.Beym@gmx.de>
2021-04-26 12:01:44 +0200

    Added warning fields to sensor slice

--- ESP32/interface/src/project/redux/reducers/sensorsSlice.js
+++ ESP32/interface/src/project/redux/reducers/sensorsSlice.js
@@ -6,12 +6,32 @@ const initialState = {
 		humidity: 0,
 		temperatureCollection: [],
 		humidityCollection: [],
+		warning: {
+			warning: {
+				enabled: false,
+				threshold: 100,
+			},
+			danger: {
+				enabled: false,
+				threshold: 100,
+			}
+		},
 	},
 	sensor2: {
 		temperature: 0,
 		humidity: 0,
 		temperatureCollection: [],
 		humidityCollection: [],
+		warning: {
+			warning: {
+				enabled: false,
+				threshold: 100,
+			},
+			danger: {
+				enabled: false,
+				threshold: 100,
+			}
+		},
 	},
 	status: 'idle',
 }
@@ -26,30 +46,40 @@ const sensorsSlice = createSlice({
 		setSensor1(state, action) {
 			let temperature = action.payload.sensor1?.temperature;
 			let humidity = action.payload.sensor1?.humidity;
-			if("temperature" in action.payload.sensor1) {
+			let warning = action.payload.sensor1?.warning;
+
+			if ("temperature" in action.payload.sensor1) {
 				state.sensor1.temperature = temperature;
 				const temperatureData = {temperature: temperature, time: new Date().getTime()};
 				state.sensor1.temperatureCollection = [...state.sensor1.temperatureCollection, temperatureData];
 			}
-			if("humidity" in action.payload.sensor1) {
+			if ("humidity" in action.payload.sensor1) {
 				state.sensor1.humidity = humidity;
 				state.sensor1.humidityCollection = [...state.sensor1.humidityCollection, humidity]
 				const humidityData = {humidity: humidity, time: new Date().getTime()};
 				state.sensor1.humidityCollection = [...state.sensor1.humidityCollection, humidityData];
 			}
+			if ("warning" in action.payload.sensor1) {
+				state.sensor1.warning = warning;
+			}
 		},
 		setSensor2(state, action) {
 			let temperature = action.payload.sensor2?.temperature;
 			let humidity = action.payload.sensor2?.humidity;
-			if("temperature" in action.payload.sensor2) {
+			let warning = action.payload.sensor2?.warning;
+
+			if ("temperature" in action.payload.sensor2) {
 				state.sensor2.temperature = temperature;
 				const temperatureData = {temperature: temperature, time: new Date().getTime()};
 				state.sensor2.temperatureCollection = [...state.sensor2.temperatureCollection, temperatureData];
 			}
-			if("humidity" in action.payload.sensor2) {
+			if ("humidity" in action.payload.sensor2) {
 				state.sensor2.humidity = humidity;
 				const humidityData = {humidity: humidity, time: new Date().getTime()};
 				state.sensor2.humidityCollection = [...state.sensor2.humidityCollection, humidityData];
+			}
+			if ("warning" in action.payload.sensor2) {
+				state.sensor2.warning = warning;
 			}
 		},
 		clearSensor1Collections(state, action) {
