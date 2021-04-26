f3034447867e55c9304103845c0a518970800657
Marvin Beym <Marvin.Beym@gmx.de>
2021-04-26 13:43:24 +0200

    Changed leds page to use SelectCard component for led2 effect.

--- ESP32/interface/src/project/tabs/Leds.js
+++ ESP32/interface/src/project/tabs/Leds.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {makeStyles} from "@material-ui/core/styles";
-import {FormControl, InputLabel, MenuItem, Select} from "@material-ui/core";
 import {useDispatch, useSelector} from "react-redux";
 import {selectLed1, selectLed2,} from "../redux/reducers/ledsSlice";
 import {wsSend} from "../redux/reducers/webSocketSlice";
@@ -9,6 +8,7 @@ import Card from "../components/Card";
 import SwitchCard from "../components/SwitchCard";
 import Led2EffectConfigForm from "../components/form/forms/Led2EffectConfigForm";
 import {motion} from "framer-motion";
+import SelectCard from "../components/SelectCard";
 
 const useStyles = makeStyles((theme) => ({
 	leds: {
@@ -40,8 +40,8 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const variants = {
-	show: { opacity: 1, y: 0 },
-	hide: { opacity: 0, y: "100%" },
+	show: {opacity: 1, y: 0},
+	hide: {opacity: 0, y: "100%"},
 }
 
 const Leds = () => {
@@ -57,31 +57,28 @@ const Leds = () => {
 		dispatch(wsSend({component: "led1", command: "setState", value: checked ? 1 : 0}));
 	}
 
-	const onLed2EffectChange = (event) => {
-		dispatch(wsSend({component: "led2", command: "setEffect", value: event.target.value}));
+	const onLed2EffectChange = (value) => {
+		dispatch(wsSend({component: "led2", command: "setEffect", value: value}));
 	}
 
 	return (
 		<TabContent className={styles.leds}>
 			<Card header="Leds">
-				<SwitchCard header="Led 1" checked={led1.state} onChange={onLed1Change}/>
-				<Card header="Led 2">
-					<FormControl fullWidth variant="filled">
-						<InputLabel id="demo-simple-select-outlined-label">Effect</InputLabel>
-						<Select value={led2.currentEffect} onChange={onLed2EffectChange}
-								className={styles.effectChooser}>
-							{led2.effects.map((effect) => (
-								<MenuItem key={effect.id} value={effect.id}>{effect.name.toUpperCase()}</MenuItem>
-							))}
-						</Select>
-					</FormControl>
-				</Card>
+				<SwitchCard header="Led 1 State" checked={led1.state} onChange={onLed1Change}/>
+				<SelectCard
+					header="Led2 Effect"
+					value={led2.currentEffect}
+					onChange={onLed2EffectChange}
+					options={led2.effects.map((effect) => {
+						return {value: effect.id, label: effect.name.toUpperCase()}
+					})}
+				/>
 			</Card>
 			<motion.div animate={currentEffectHasConfig ? "show" : "hide"} variants={variants}>
 				<Card header="Effect configuration">
 					{currentEffectHasConfig ?
 						<Led2EffectConfigForm effect={currentEffect}/>
-					: null}
+						: null}
 				</Card>
 			</motion.div>
 		</TabContent>
